8835dc4aebefa42f4c7f129fedc155a9d0880489
Miguel Escobar <mae2136@gmail.com>
2022-06-17 23:26:25 -0500

    Create Pages structure. Start linking components.

--- me-react-portfolio/src/App.js
+++ me-react-portfolio/src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Header from "./components/Header"
+import PortfolioContainer from './components/PortfolioContainer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <PortfolioContainer />
   );
 }
 
