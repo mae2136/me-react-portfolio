dd31495f80fb64a858fbc589dcf68d5fef2a45ad
Miguel Escobar <mae2136@gmail.com>
2022-06-21 23:04:15 -0500

    Add readme. Update styling for all components.

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,7 +7,8 @@ function Resume() {
         <>
             <h2 className="justify-center flex flex-row py-2">Resume</h2>
             <div className="flex flex-row justify-center">
-                <embed className="py-1 rounded border-gray-800 drop-shadow h-80 w-80 md:min-h-screen md:min-w-full" src={process.env.PUBLIC_URL + '/Escobar_Resume.pdf'} />
+                <embed className="hidden md:flex py-1 rounded border-gray-800 drop-shadow h-80 w-80 md:min-h-screen md:min-w-full" src={process.env.PUBLIC_URL + '/Escobar_Resume.pdf'} />
+                <button>Download Resume</button>
             </div>
         </>
     )
