34d27e107b2046096c6f06854c4ea5e4592cc0fb
Miguel Escobar <mae2136@gmail.com>
2022-06-23 23:06:42 -0500

    Added Logos to footer. Updated Resume page to include skills.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,23 @@
 import React from "react";
+import twitterLogo from "../assets/Twitter-Logo.png";
+import githubLogo from "../assets/GitHub-Mark-Light-64px.png";
+import linkedInLogo from "../assets/LI-In-Bug.png";
 
-// const navTitles = ["ABOUT ME", "PORTFOLIO", "CONTACT", "RESUME"];
+// https://www.w3schools.com/tags/att_a_target.asp
 
 function Footer() {
     return (
         <footer className="flex flex-row justify-center p-3 bg-cyan-600">
-            <h3 className="text-slate-200 text-xl">@Miguel Escobar, 2022</h3>
+            {/* <h3 className="text-slate-200 text-xl">@Miguel Escobar, 2022</h3> */}
+            <a className="m-3" href="https://github.com/mae2136" target="_blank">
+                <img src={githubLogo} alt="GitHub Logo"></img>
+            </a>
+            <a className="m-3" href="https://www.linkedin.com/in/miguel-a-escobar/" target="_blank">
+                <img className="max-h-16" src={linkedInLogo} alt="LinkedIn Logo"></img>
+            </a>
+            <a className="m-3" href="https://twitter.com/angelarchero" target="_blank">
+                <img className="max-h-16" src={twitterLogo} alt="Twitter Logo"></img>
+            </a>
         </footer>
     )
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,15 +1,33 @@
 import React from "react";
 
-// const navTitles = ["ABOUT ME", "PORTFOLIO", "CONTACT", "RESUME"];
+const frontEnd = ["Javascript", "CSS", "ReactJS", "Handlebars", "Tailwind CSS", "Bootstrap", "HTML", "PWA & Service Workers"];
+const backEnd = ["mySQL/Sequelize", "MongoDB/Mongoose", "Express", "Node.js", "RESTful API Routes", "Apollo/GraphQL"];
 
 function Resume() {
     return (
         <>
             <h2 className="font-bold text-slate-200 justify-center text-3xl flex flex-row mb-3 border-b pb-2">Resume</h2>
-            <div className="flex md:flex-row flex-col justify-center">
-                <embed className="hidden md:flex py-1 rounded border-gray-800 drop-shadow h-80 w-80 md:min-h-screen md:min-w-full" src={process.env.PUBLIC_URL + '/Escobar_Resume.pdf'} />
-                <p className="md:hidden text-slate-200 mb-3 text-xl text-center">Click the button below to download and read my resume</p>
-                <a className="text-center md:hidden text-slate-200 p-2 rounded bg-gray-800 mb-2" href={process.env.PUBLIC_URL + '/Escobar_Resume.pdf'} download="escobar_resume.pdf">Download Resume</a>
+            <div className="flex flex-col justify-center md:justify-start">
+                {/* <embed className="hidden md:flex py-1 rounded border-gray-800 drop-shadow h-80 w-80 md:min-h-screen md:min-w-full" src={process.env.PUBLIC_URL + '/Escobar_Resume.pdf'} /> */}
+                <p className="text-slate-200 mb-3 text-xl text-center">Click the button below to download and read my resume</p>
+                <a className="text-center text-slate-200 p-2 rounded bg-gray-800 mb-2" href={process.env.PUBLIC_URL + '/Escobar_Resume.pdf'} download="escobar_resume.pdf">Download Resume</a>
+                <div className="text-center text-slate-200 p-2 min-w-fit block">
+                    <h3 className="font-bold text-2xl border-b mb-2">Front-End Skills</h3>
+                    <ul>
+                        {frontEnd.map((skill, index) => {
+                            return <li className="text-slate-200 mb-3 text-xl" key={index}>{skill}</li>
+                        })}
+                    </ul>
+                </div>
+                <div className="text-center text-slate-200 p-2 min-w-fit block">
+                    <h3 className="font-bold text-2xl border-b mb-2">Back-End Skills</h3>
+                    <ul>
+                        {backEnd.map((skill, index) => {
+                            return <li className="text-slate-200 mb-3 text-xl text-center" key={index}>{skill}</li>
+                        })}
+                    </ul>
+                </div>
+                
             </div>
         </>
     )
